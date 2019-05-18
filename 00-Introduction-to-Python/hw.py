a067ceebd193ef83fb1c3232d565131944c93f22
Evgeniy Myshevskiy <mr.bonzzomi@gmail.com>
2019-05-18 08:58:00 +0300

    00 hw solved

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -9,7 +9,14 @@
 
 def is_permutation(a: str, b: str) -> bool:
     # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
-
+    if len(a) != len(b):
+    	return False
+    a_sorted = sorted(a)
+    b_sorted = sorted(b)
+    for i in range(len(a_sorted)):
+    	if a_sorted[i] != b_sorted[i]:
+    		return False
+    return True
 
 assert is_permutation('baba', 'abab')
 assert is_permutation('abbba', 'abab')
