f6f2dfea50e48a91bcf80e7a2d3fc13993786db3
leemvale <neuromiwork@gmail.com>
2019-06-17 17:54:58 +0300

    07 hw: client and server done

---
+++ 07-networks-basics/client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+import select
+import socket
+import sys
+
+
+if __name__ == "__main__":
+    host, port = '127.0.0.1', 9999
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server_socket.connect((host, port))
+    except ConnectionRefusedError:
+        print('\nUnable to connect')
+        sys.exit()
+    print('\nConnected to TCP chat server')
+    try:
+        while True:
+            socket_list = [sys.stdin, server_socket]
+            read_sockets, _, _ = select.select(socket_list, [], [])
+            for sock in read_sockets:
+                if sock == server_socket:
+                    data = sock.recv(2048).decode('utf-8').rstrip()
+                    if not data:
+                        server_socket.close()
+                        print('\nDisconnected from chat server')
+                        sys.exit()
+                    else:
+                        print(data)
+                else:
+                    msg = sys.stdin.readline()
+                    server_socket.send(bytes(msg, 'utf-8'))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server_socket.close()
+        print('\nClient stoped')
---
+++ 07-networks-basics/server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+
+import select
+import socket
+
+
+def send_to_all(client_socket, message):
+    for dest_socket in clients:
+        if dest_socket != main_socket and dest_socket != client_socket:
+            try:
+                dest_socket.send(message)
+            except BrokenPipeError:
+                continue
+
+
+if __name__ == "__main__":
+    host, port = '', 9999
+    clients = {}
+    main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    main_socket.bind((host, port))
+    main_socket.listen(10)
+    clients[main_socket] = f"('{host}', {port})"
+
+    print(f'TCP chat server started on {port} port')
+    try:
+        while True:
+            read_sockets, _, _ = select.select(clients, [], [])
+            for sock in read_sockets:
+                if sock == main_socket:
+                    client_socket, client_addr = main_socket.accept()
+                    print(f'{client_addr} connected')
+                    clients[client_socket] = f'{client_addr}'
+                else:
+                    try:
+                        data = sock.recv(2048)
+                    except OSError:
+                        continue
+                    if not data:
+                        print(f'{clients[sock]} disconnected')
+                        sock.close()
+                        del clients[sock]
+                    else:
+                        client_addr = f'{clients[sock]}: '.encode('utf-8')
+                        data = client_addr + data
+                        send_to_all(sock, data)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for sock in clients:
+            sock.close()
+        print('\nTCP chat server stoped')
