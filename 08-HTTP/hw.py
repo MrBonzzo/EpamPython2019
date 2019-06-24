5115b2bdc7fe8fae8890fc6953723ba99fbbde2b
Evgeniy Myshevskiy <31772882+MrBonzzo@users.noreply.github.com>
2019-06-24 21:37:29 +0300

    08 hw: pikabu parser done

--- 08-HTTP/hw.py
+++ 08-HTTP/hw.py
@@ -1,12 +1,21 @@
-import requests
 from bs4 import BeautifulSoup
-import re
-import json
-import time
 from collections import Counter
+import re
+import requests
+import time
+import json
 
+
+def get_cookies_from_file(filename):
+    with open(filename, 'r') as cookie_file:
+        lines = [c.strip() for c in cookie_file.read().split('\n')]
+        cookies = {lines[0]: lines[1], lines[2]: lines[3]}
+    return cookies
+
+print(get_cookies_from_file('Cookies'))
 
 class PikabuGrabber():
+    cookies = get_cookies_from_file('Cookies')
     HEADERS_MAIN_PAGE = {
         "Host": "pikabu.ru",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; "
@@ -17,30 +26,7 @@ class PikabuGrabber():
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive",
         "Referer": "https://pikabu.ru/",
-        "Cookie": "PHPSESS=ouoo8b36mu9vlvt51ou0fooo9aea3go5; "
-                  "is_scrollmode=1; pcid=VuZjWR0Zhv2; ulfs=1561372327; "
-                  "_vA3=P5-w-C-C-90ZQ-KCaQ.t4-d-A-B-90ZQ-1NaQ."
-                  "X4-w-C-C-A_ZQ-PCaQ.4a8yMB; "
-                  "rheftjdd=rheftjddVal; pkb_modern=11; "
-                  "_ym_uid=1561364278671438861; "
-                  "_ym_d=1561364278; bs=G0; _ga=GA1.2.799294783.1561364279; "
-                  "_gid=GA1.2.1965745844.1561364279; _ym_isad=2; "
-                  "vn=eJwdybcRADAIBLCFXBA+sP9kPqhUSLaK+WTDqLMz1g7O"
-                  "SumebXzljAmR; "
-                  "cto_lwid=885ad351-4ecc-4503-91dc-572e4f9268be; "
-                  "fps=d0cb5eec6656a5774a8628d19df6341197; "
-                  "__utma=43320484.799294783.1561364279.1561364341."
-                  "1561364341.1; "
-                  "__utmc=43320484; "
-                  "__utmz=43320484.1561364341.1.1.utmcsr=(direct)|"
-                  "utmccn=(direct)|utmcmd=(none); "
-                  "autohide_news=0; _nuser=1; nps7s=2149264246; "
-                  "_ym_visorc_174977=w; "
-                  "_ym_visorc_56459=w; set_autohide_news=-1; la=1; "
-                  "phpDug2=a%3A4%3A%7Bs%3A3%3A%22uid%22%3Bs%3A7%3A%222795727%"
-                  "22%3Bs%3A8%3A%22username%22%3Bs%3A8%3A%22Bonzzomi%22%3Bs%3"
-                  "A3%3A%22rem%22%3Bs%3A32%3A%225b0faa5498b47b1098a11f60f6498"
-                  "be1%22%3Bs%3A5%3A%22tries%22%3Bi%3A0%3B%7D",
+        "Cookie": cookies['Cookie_main_page'],
         "Upgrade-Insecure-Requests": "1",
         "Cache-Control": "max-age=0",
         "TE": "Trailers"}
@@ -56,26 +42,7 @@ class PikabuGrabber():
         "X-Requested-With": "XMLHttpRequest",
         "Connection": "keep-alive",
         "Referer": "https://pikabu.ru/",
-        "Cookie": "PHPSESS=ouoo8b36mu9vlvt51ou0fooo9aea3go5; "
-                  "is_scrollmode=1; pcid=VuZjWR0Zhv2; ulfs=1561381851; "
-                  "_vA3=X4-w-B-B-A_ZQ-eGaQ.t4-d-A-B-90ZQ-1NaQ.Q5-d-C-C-RDaQ-"
-                  "dGaQ.P5-w-B-B-90ZQ-dGaQ.z0mK1C; rheftjdd=rheftjddVal; "
-                  "pkb_modern=11; _ym_uid=1561364278671438861; "
-                  "_ym_d=1561364278; bs=G0; _ga=GA1.2.799294783.1561364279; "
-                  "_gid=GA1.2.1965745844.1561364279; _ym_isad=2; "
-                  "vn=eJwlybENACAMA8GFKEIS27D/YiimeumPEi9yUepoTos6Uya2v/p77bA"
-                  "H7CDtKPUDCwgOfQ==; cto_lwid=885ad351-4ecc-4503-91dc-572e"
-                  "4f9268be; fps=d0cb5eec6656a5774a8628d19df6341197; "
-                  "__utma=43320484.799294783.1561364279.1561364341."
-                  "1561364341.1; __utmc=43320484; __utmz=43320484.1561364341."
-                  "1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); "
-                  "autohide_news=0; _nuser=1; nps7s=2149264246; "
-                  "set_autohide_news=-1; phpDug2=a%3A4%3A%7Bs%3A3%3A%22uid"
-                  "%22%3Bs%3A7%3A%222795727%22%3Bs%3A8%3A%22username%22%3B"
-                  "s%3A8%3A%22Bonzzomi%22%3Bs%3A3%3A%22rem%22%3Bs%3A32%3A%"
-                  "225b0faa5498b47b1098a11f60f6498be1%22%3"
-                  "Bs%3A5%3A%22tries%22%3Bi%3A0%3B%7D; "
-                  "la=1; _gat_gtag_UA_28292940_1=1"}
+        "Cookie": cookies['Cookie_loading']}
 
     def __init__(self):
         self.session = requests.Session()
