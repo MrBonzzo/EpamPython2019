afd82e4d5b54510c34d5e8ca6157ad04f57969a9
Evgeniy Myshevskiy <e.g.myshevskiy@gmail.com>
2019-07-03 16:51:16 +0300

    10 hw: observer done

--- 10-Design_Patterns/hw/3-Observer/observer.py
+++ 10-Design_Patterns/hw/3-Observer/observer.py
@@ -49,3 +49,63 @@ Dear John, there is new playlist on 'All about dogs' channel: 'Dogs nutrition'
 Dear Erica, there is new playlist on 'All about dogs' channel: 'Dogs nutrition'
 
 """
+import logging
+
+# logging.basicConfig(level=logging.DEBUG)
+
+
+class MyTubeChannel:
+    def __init__(self, channel_name, chanel_owner):
+        self.name = channel_name
+        self.owner = chanel_owner
+        self.playlist = {'without playlist': []}
+        self.subscribers = []
+
+    def subscribe(self, user):
+        self.subscribers.append(user)
+
+    def unsubscribe(self, user):
+        if user in self.subscribers:
+            self.subscribers.remove(user)
+
+    def notify(self, data):
+        for sub in self.subscribers:
+            sub.update(f'Dear {sub._name}, {data}')
+
+    def publish_video(self, video):
+        self.playlist['without playlist'].append(video)
+        msg = f"there is new video on '{self.name}' chanel: '{video}'"
+        self.notify(msg)
+
+    def publish_playlist(self, playlist):
+        name = list(playlist)[0]
+        self.playlist[name] = playlist[name]
+        msg = f"there is new playlist on '{self.name}' chanel: '{name}'"
+        self.notify(msg)
+
+
+class MyTubeUser:
+    def __init__(self, user_name):
+        self._name = user_name
+
+    def update(self, msg):
+        print(msg)
+
+
+if __name__ == '__main__':
+    matt = MyTubeUser('Matt')
+    john = MyTubeUser('John')
+    erica = MyTubeUser('Erica')
+
+    dogs_life = MyTubeChannel('All about dogs', matt)
+    dogs_life.subscribe(john)
+    dogs_life.subscribe(erica)
+
+    dogs_nutrition_videos = ['What do dogs eat?',
+                             'Which Pedigree pack to choose?']
+    dogs_nutrition_playlist = {'Dogs nutrition': dogs_nutrition_videos}
+
+    for video in dogs_nutrition_videos:
+        dogs_life.publish_video(video)
+
+    dogs_life.publish_playlist(dogs_nutrition_playlist)
